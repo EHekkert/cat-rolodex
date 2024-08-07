67f2f3f2533b5d9984715a6f490c4ece19f65b3b
Hekkert, Erwin <erwin.hekkert@tauw.com>
2024-08-07 14:44:47 +0200

    minor changes

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,16 +1,14 @@
 import Card from '../card/card.component';
 import './card-list.styles.css';
 
-const CardList = ({ itemAltPrefix, listItems }) => {
-    return (
-        <div className='card-list'>
-            {listItems.map((item) => {
-                return (
-                    <Card key={item.id} itemAltPrefix={itemAltPrefix} item={item} />
-                )
-            })}
-        </div>           
-    );
-}
+const CardList = ({ itemAltPrefix, listItems }) => (
+    <div className='card-list'>
+        {listItems.map((item) => {
+            return (
+                <Card key={item.id} itemAltPrefix={itemAltPrefix} item={item} />
+            )
+        })}
+    </div>           
+)
 
 export default CardList;
